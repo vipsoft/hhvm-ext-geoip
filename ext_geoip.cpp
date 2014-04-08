0e84c07626554e13c03f1726d671d2e0c142b73c
root <root@hhvm-dev>
2014-04-08 17:53:23 +0000

    Add GeoIP_internal.h - missing from 12.04's libgeoip-dev
    Implemented:
    * geoip_database_info()
    * geoip_db_avail()
    * geoip_db_filename()
    * geoip_db_get_all_info()
    * geoip_id_by_name()
    * geoip_region_name_by_code()
    * geoip_time_zone_by_country_and_region()
    
    Note:
    - tests/014.phpt currently fails here and in the geoip PECL extension v1.0.8

--- ext_geoip.cpp
+++ ext_geoip.cpp
@@ -22,6 +22,7 @@
 #include <cinttypes>
 #include <GeoIP.h>
 #include <GeoIPCity.h>
+#include "GeoIP_internal.h"
 
 namespace HPHP {
 
@@ -73,18 +74,79 @@ static Variant HHVM_FUNCTION(geoip_country_name_by_name, const String& hostname)
 }
 
 static Variant HHVM_FUNCTION(geoip_database_info, int64_t database /* = GEOIP_COUNTRY_EDITION */) {
-    return null_variant;
+    GeoIP *gi;
+    char *db_info;
+
+    if (database < 0 || database >= NUM_DB_TYPES) {
+        raise_warning("geoip_database_info(): Database type given is out of bound.");
+
+        return null_variant;
+    }
+
+    if ( ! GeoIP_db_avail(database)) {
+        if (NULL != GeoIPDBFileName[database]) {
+            raise_warning("geoip_database_info(): Required database not available at %s.", GeoIPDBFileName[GEOIP_COUNTRY_EDITION]);
+        } else {
+            raise_warning("geoip_database_info(): Required database not available.");
+        }
+
+        return null_variant;
+    }
+
+    gi = GeoIP_open_type(database, GEOIP_STANDARD);
+    db_info = GeoIP_database_info(gi);
+    GeoIP_delete(gi);
+
+    return Variant(String(db_info));
 }
 
 static Variant HHVM_FUNCTION(geoip_db_avail, int64_t database) {
-    return null_variant;
+    if (database < 0 || database >= NUM_DB_TYPES) {
+        raise_warning("geoip_db_avail(): Database type given is out of bound.");
+
+        return null_variant;
+    }
+
+    return Variant((bool) GeoIP_db_avail(database));
 }
 
 static Variant HHVM_FUNCTION(geoip_db_filename, int64_t database) {
-    return null_variant;
+    const char *filename;
+
+    if (database < 0 || database >= NUM_DB_TYPES) {
+        raise_warning("geoip_db_filename(): Database type given is out of bound.");
+
+        return null_variant;
+    }
+
+    filename = GeoIPDBFileName[database];
+
+    if (filename == NULL) {
+        return null_variant;
+    }
+
+    return Variant(String(filename));
 }
 
 static Array HHVM_FUNCTION(geoip_db_get_all_info) {
+    Array info = Array::Create();
+
+    for (int i = 0; i < NUM_DB_TYPES; i++) {
+        if (NULL != GeoIPDBDescription[i]) {
+            Array row = Array::Create();
+
+            row.add(String("available"), Variant((bool) GeoIP_db_avail(i)));
+            row.add(String("description"), Variant(String(GeoIPDBDescription[i])));
+
+            if (GeoIPDBFileName[i]) {
+                row.add(String("filename"), Variant(String(GeoIPDBFileName[i])));
+            }
+
+            info.set(i, Variant(row));
+        }
+    }
+
+    return info;
 }
 
 static Variant HHVM_FUNCTION(geoip_id_by_name, const String& hostname) {
@@ -108,11 +170,50 @@ static Variant HHVM_FUNCTION(geoip_region_by_name, const String& hostname) {
 }
 
 static Variant HHVM_FUNCTION(geoip_region_name_by_code, const String& country_code, const String& region_code) {
-    return null_variant;
+    const char *region_name;
+
+    if ( ! country_code.length() || ! region_code.length()) {
+        raise_warning("geoip_region_name_by_code(): You need to specify the country and region codes.");
+
+        return Variant(false);
+    }
+
+    region_name = GeoIP_region_name_by_code(country_code.c_str(), region_code.c_str());
+
+    if (region_name == NULL) {
+        return Variant(false);
+    }
+
+    return Variant(String(region_name));
 }
 
-static Variant HHVM_FUNCTION(geoip_time_zone_by_country_and_region, const String& country_code, const String& region_code) {
-    return null_variant;
+static Variant HHVM_FUNCTION(geoip_time_zone_by_country_and_region, const String& country_code, const Variant& region_code) {
+    const char *timezone;
+    const char *region;
+
+    if ( ! country_code.length()) {
+        raise_warning("geoip_time_zone_by_country_and_region(): You need to specify at least the country code.");
+
+        return Variant(false);
+    }
+
+    if (region_code.isNull()) {
+        region = NULL;
+    } else if (region_code.isString()) {
+        region = region_code.toString().c_str();
+    } else {
+        raise_param_type_warning("geoip_time_zone_by_country_and_region", 2, KindOfString, region_code.getType());
+
+        return null_variant;
+    }
+
+    timezone = GeoIP_time_zone_by_country_and_region(country_code.c_str(), region);
+
+    if (timezone == NULL) {
+        return Variant(false);
+    }
+
+    return Variant(String(timezone));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -153,6 +254,8 @@ public:
     HHVM_FE(geoip_time_zone_by_country_and_region);
 
     loadSystemlib();
+
+    _GeoIP_setup_dbfilename();
   }
 } s_geoip_extension;
 
