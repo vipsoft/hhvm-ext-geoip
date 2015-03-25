aca3a9615e3e676a70c610828e86977f6d5e0a09
Sebastian Lagemann <sl@honeytracks.com>
2015-03-25 14:53:49 +0100

    fixed static command path for hphpize to make it better usable on systems which have different directory structures

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-if [[ "$HPHP_HOME" == "" ]]; then
-    echo HPHP_HOME environment variable must be set!
-    exit 1
+if [ "$HPHPIZE" == "" ]; then
+	HPHPIZE=hphpize
+fi
+if [ "$HPHP_HOME" != "" ] && [ "$HPHPIZE" == "hphpize" ]; then
+	HPHPIZE=$HPHP_HOME/hphp/tools/hphpize/hphpize
 fi
 
-$HPHP_HOME/hphp/tools/hphpize/hphpize
+if ! type "$HPHPIZE" > /dev/null; then
+	echo "$HPHPIZE doesn't exist, please specify with HPHPIZE=<path to hphpize> or use HPHP_HOME"
+	exit 127;
+fi
+
+$HPHPIZE
 cmake .
 make
