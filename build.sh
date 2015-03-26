be44647515b9dc98957ede30df59ef56a25f3fc9
Anthon Pang <anthon.pang@gmail.com>
2015-03-26 00:24:19 +0000

    Closing #23 in favor of this patch

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-if [[ "$HPHP_HOME" == "" ]]; then
-    echo HPHP_HOME environment variable must be set!
+if [ "$HPHP_HOME" != "" ]; then
+    HPHPIZE=$HPHP_HOME/hphp/tools/hphpize/hphpize
+else
+    HPHPIZE=`which hphpize`
+fi
+
+if [ ! -e "$HPHPIZE" ]; then
+    echo 'hphpize not found. Try setting the HPHP_HOME environment variable.'
     exit 1
 fi
 
-$HPHP_HOME/hphp/tools/hphpize/hphpize
+$HPHPIZE
 cmake .
 make
